9b401a384c96893eaa33d217a118ddad33beb8da
supernovahs <harshitsinghal252@gmail.com>
2023-01-07 01:02:49 +0530

    isContract in yul

---
+++ src/utils/Address.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.15;
+
+library Address {
+
+// @notice Yul implementation of isContract 
+    function isContract(address _account) internal view returns (bool success){
+        assembly{
+            let x :=0
+            x:= extcodesize(_account)
+            success:= gt(x,0)
+        }
+    }
+
+}
